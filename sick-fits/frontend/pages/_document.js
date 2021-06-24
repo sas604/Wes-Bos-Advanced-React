c5515c1c6819e8240c365852e5d61dee0ddb241c
Aleksandr Tagunov <tagunovalexander@gmail.com>
2021-06-23 20:53:18 -0700

    Add server side style sheet

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,6 +1,16 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProp({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTag = sheet.getStyleElement();
+    return { ...page, styleTag };
+  }
+
   render() {
     return (
       <Html lang="en-US">
