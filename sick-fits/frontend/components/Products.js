c73936e65d56c063a7375c5dcc4ddecf15e87709
Aleksandr Tagunov <tagunovalexander@gmail.com>
2021-08-22 14:34:31 -0700

    Finish video 35

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,11 +1,13 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCT_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       description
@@ -25,8 +27,13 @@ const ProductsList = styled.div`
   gap: 60px;
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCT_QUERY);
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCT_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error {error}</p>;
@@ -40,3 +47,7 @@ export default function Products() {
     </div>
   );
 }
+
+Products.propTypes = {
+  page: PropTypes.number,
+};
---
+++ sick-fits/frontend/lib/paginationField.js
@@ -0,0 +1,47 @@
+import { PAGINATION_QUERY } from '../components/Pagination';
+
+export default function paginationField() {
+  return {
+    keyArgs: false,
+
+    read(existing = [], { args, cache }) {
+      console.log({ existing, args, cache });
+      const { skip, first } = args;
+      // Read the numbers of items on the page from the cache
+      const data = cache.readQuery({ query: PAGINATION_QUERY });
+      const count = data?._allProductsMeta?.count;
+      const page = skip / first + 1;
+      const pages = Math.ceil(count / first);
+
+      // Check if we have existing items
+      const items = existing.slice(skip, skip + first).filter((item) => item);
+      // If there are items  and aren't enough items and we are on the last page
+      // Then Just send it
+      if (items.length && items.length !== first && page === pages) {
+        return items;
+      }
+      if (items.length !== first) {
+        // We don't have any items, we must go to the network to fetch them
+        return false;
+      }
+
+      // If there are items, just return them from the cache, and we don't need to go to the network
+      if (items.length) {
+        console.log(items.length);
+        return items;
+      }
+      return false;
+    },
+    merge(existing, incoming, { args }) {
+      const { skip } = args;
+      // This runs when appolo client returns from network
+      const merged = existing ? existing.slice(0) : [];
+      // eslint-disable-next-line
+      for (let i = skip; i < skip + incoming.length; i++) {
+        merged[i] = incoming[i - skip];
+      }
+      console.log(merged);
+      return merged;
+    },
+  };
+}
